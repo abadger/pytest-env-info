da7d0534ba60a91c02f8af3cd361ab05c1e137f1
Toshio Kuratomi <a.badger@gmail.com>
2017-11-25 14:31:05 -0800

    Bump release

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def read(fname):
 
 setup(
     name='pytest-env-info',
-    version='0.3.0',
+    version='0.4.0',
     author='Toshio Kuratomi',
     author_email='a.badger@gmail.com',
     maintainer='Toshio Kuratomi',
